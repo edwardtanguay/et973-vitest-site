7f96e0c789da606b418b76a2472b4c8bf9f3ca1c
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 11:41:01 +0200

    222

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import * as qstr from './tools/qstr';
 
-const message = 'here is a text';
+const message = 'here is a text222';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-3xl">Vitest Site</h1>
