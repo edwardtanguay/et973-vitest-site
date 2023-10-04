3c50c47c571cff843efd64f505f35549e657c5b6
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 11:56:02 +0200

    trim taken out

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import * as qstr from './tools/qstr';
 
-const message = 'this is a test: all test pass';
+const message = 'this is a test: version no-trim';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-3xl">Vitest Site</h1>
