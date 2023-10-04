ba8e75abc5733fc64b2d4efc227fac8269a7f3d3
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 11:54:12 +0200

    all test pass

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import * as qstr from './tools/qstr';
 
-const message = 'this is a number: 4444';
+const message = 'this is a test: all test pass';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-3xl">Vitest Site</h1>
