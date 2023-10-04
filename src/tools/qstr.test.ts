d7e35284c2027b7ea88b93b55552ffb9b39fa98d
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 11:34:53 +0200

    first function tests finished

--- src/tools/qstr.test.ts
+++ src/tools/qstr.test.ts
@@ -5,9 +5,18 @@ describe('capitalizeFirstLetter()', () => {
 		expect(capitalizeFirstLetter('testing')).toBe('Testing');
 		expect(capitalizeFirstLetter('TESTING')).toBe('TESTING');
 		expect(capitalizeFirstLetter('T8282829383')).toBe('T8282829383');
+		expect(capitalizeFirstLetter('two words')).toBe('Two words');
 	});
 	it('handles edge cases', () => {
 		expect(capitalizeFirstLetter(' testing')).toBe('Testing');
-		// expect(capitalizeFirstLetter('')).toBe('Testing');
+		expect(capitalizeFirstLetter('')).toBe('');
+	});
+	it('handles German letters', () => {
+		expect(capitalizeFirstLetter('äpeln')).toBe('Äpeln');
+		expect(capitalizeFirstLetter('öfter')).toBe('Öfter');
+		expect(capitalizeFirstLetter('über')).toBe('Über');
+		expect(capitalizeFirstLetter('ÄÄÄÄ')).toBe('ÄÄÄÄ');
+		expect(capitalizeFirstLetter('ÖÖÖÖ')).toBe('ÖÖÖÖ');
+		expect(capitalizeFirstLetter('ÜÜÜÜ')).toBe('ÜÜÜÜ');
 	});
 });
