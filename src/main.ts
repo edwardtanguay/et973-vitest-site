6807200399df9bae7dfefce91f6d354ab86a0751
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 10:52:28 +0200

    import as added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css';
-import { capitalizeFirstLetter } from './tools/qstr';
+import * as qstr from './tools/qstr';
 
 const message = 'this is a test';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-3xl">Vitest Site</h1>
-<p>${capitalizeFirstLetter(message)}</p>
+<p>${qstr.capitalizeFirstLetter(message)}</p>
 `;
