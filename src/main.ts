23b00525ccee81a77bd13eac92eac285a7083e9e
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-04 11:49:25 +0200

    fixed 4444 error

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import * as qstr from './tools/qstr';
 
-const message = 4444;
+const message = 'this is a number: 4444';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1 class="text-3xl">Vitest Site</h1>
